c5625df43ebc972fc91b9ad7544320617a561d12
miphreal <miphreal@gmail.com>
2022-11-04 18:25:48 +0100

    Fix i3 and rofi configs

--- tools/i3/.confbuild.py
+++ tools/i3/.confbuild.py
@@ -4,32 +4,31 @@ def i3(ctl):
 
     ctl.conf(
         i3_config_dir="~/.config/i3",
-        i3_config="{{ i3_config_dir }}/config",
         bg_image="{{ i3_config_dir }}/bg.png",
-        statusbar_config="{{ i3_config_dir }}/statusbar.py",
         I3_STATUS_VENV_PYTHON_BIN="~/.pyenv/versions/i3status/bin/python",
         ROFI_BIN="rofi",
+        statusbar_config="{{ i3_config_dir }}/statusbar.py",
     )
 
     ctl.ensure_dirs(ctl.i3_config_dir)
 
     if "full" in ctl.ctx:
-        ctl.sh(
-            # i3 deps
-            "sudo apt-get install -y i3 compton fonts-font-awesome xbacklight > /dev/null",
-        )
+        ctl.sudo("apt-get install -y i3 compton fonts-font-awesome xbacklight")
 
-    venvs = ctl.sh("pyenv virtualenvs --bare")[0]
-    if not b"i3status" in venvs:
-        ctl.sh("pyenv virtualenv 3.8.12 i3status")
+    venvs = ctl.sh("pyenv virtualenvs --bare")
+    if all("i3status" not in vname for vname in venvs):
+        ctl.sh("pyenv virtualenv 3.8.13 i3status")
         ctl.sh(
             # i3pystatus deps
-            f"{ctl.I3_STATUS_VENV_PYTHON_BIN} -m pip install --quiet git+https://github.com/enkore/i3pystatus.git",
+            f"{ctl.I3_STATUS_VENV_PYTHON_BIN} -m pip install --quiet git+https://github.com/enkore/i3pystatus.git"
+        )
+        ctl.sh(
             f"{ctl.I3_STATUS_VENV_PYTHON_BIN} -m pip install --quiet xkbgroup netifaces colour psutil",
         )
 
     # ctl.copy("./bg.png", ctl.bg_image)
-    ctl.render("config.j2", ctl.i3_config)
-    ctl.render("i3pystatusbar.py.j2", ctl.statusbar_config)
+    ctl.render("config.j2", "{{ i3_config_dir }}/config")
+    ctl.render("i3pystatusbar.py.j2", "{{ statusbar_config }}")
 
-    ctl.sh("i3-msg restart")
+    if "no-restart" not in ctl.ctx:
+        ctl.sh("i3-msg restart")
--- tools/rofi/.confbuild.py
+++ tools/rofi/.confbuild.py
@@ -1,30 +1,42 @@
+from pathlib import Path
+
+
 def rofi(ctl):
     if not ctl.is_linux:
         return
 
     ctl.conf(
-        ROFI_FONT="Roboto Mono for Powerline 11",
-        ROFI_MODES="window,drun,run,ssh,menu:~/.local/bin/rofi-custom-menu",
-        ROFI_OPACITY=100,
-        ROFI_WIDTH=30,
-        ROFI_COLUMNS=1,
-        ROFI_VENV_PYTHON_BIN="~/.pyenv/versions/rofi/bin/python",
-        rofi_config="~/.config/rofi/config.rasi",
-        rofi_custom_menu_bin="~/.local/bin/rofi-custom-menu",
+        i3_lock_background_image="{{ user.home }}/.config/i3/bg.png",
+        rofi_venv_python_bin="{{ user.home }}/.pyenv/versions/rofi/bin/python",
+        rofi_font="Roboto Mono for Powerline 11",
+        rofi_modes="window,drun,run,ssh,menu:{{ rofi_custom_menu_bin }}",
+        rofi_config_dir="{{ user.config }}/rofi",
+        rofi_config="{{ rofi_config_dir }}/config.rasi",
+        rofi_custom_menu_bin="{{ user.bin }}/rofi-custom-menu",
+        # Rofi theme
+        themes_repo="https://github.com/lr-tech/rofi-themes-collection",
+        themes_repo_dir="{{ rofi_config_dir }}/themes-repo",
+        themes_dir="{{ rofi_config_dir }}/themes",
+        rofi_theme=["squared-everforest", "gruvbox-light-soft"][0],
     )
 
-    ctl.sh("sudo apt-get install -y rofi > /dev/null")
+    if not ctl.sh("which rofi"):
+        ctl.sudo("apt-get install -y rofi")
 
     ctl.ensure_dirs("~/.config/rofi")
     # Create venv
-    venvs = ctl.sh("pyenv virtualenvs --bare")[0]
-    if not b"rofi" in venvs:
-        ctl.sh("pyenv virtualenv 3.8.12 rofi")
+    venvs = ctl.sh("pyenv virtualenvs --bare")
+    if all("rofi" not in vname for vname in venvs):
+        ctl.sh("pyenv virtualenv 3.8.13 rofi")
         # ctl.sh(f"{ctl.ROFI_VENV_PYTHON_BIN} -m pip install rofi-menu")
         ctl.sh(
             "ln -s ~/Develop/rofi-menu/rofi_menu ~/.pyenv/versions/rofi/lib/python3.8/site-packages/rofi_menu"
         )
 
+    if not Path(ctl.themes_repo_dir).exists():
+        ctl.sh("git clone {{ themes_repo }} {{ themes_repo_dir }}")
+        ctl.sh("cp -R {{ themes_repo_dir }}/themes {{ themes_dir }}")
+
     ctl.render("./rofi.conf.rasi.j2", ctl.rofi_config)
-    ctl.render("./rofi_custom_menu.py.j2", ctl.rofi_custom_menu_bin)
+    ctl.render("./rofi_custom_menu.py.j2", "{{ rofi_custom_menu_bin }}")
     ctl.sh("chmod +x {{ rofi_custom_menu_bin }}")
