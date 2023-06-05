1249be283c361c0a747c93a99569f3378adddfa4
Evgeny Lychkovsky <miphreal@gmail.com>
2023-06-05 20:24:32 +0200

    kitty: tabs management

--- macos/kitty/.confbuild.py
+++ macos/kitty/.confbuild.py
@@ -14,7 +14,8 @@ def kitty(conf):
     if not conf.themes_dir.exists():
         conf.sh("git clone --depth 1 https://github.com/dexpota/kitty-themes.git {{ themes_dir }}")
 
-    conf.sh(f"cp -r {Path(__file__).parent}/icons/* {conf.kitty_conf.parent}")
+    conf.sh("cp -r {{ current_config_dir }}/icons/* {{ kitty_conf.parent }}")
+    conf.sh("cp {{ current_config_dir }}/tab_bar.py {{ kitty_conf.parent }}")
 
     # reload kitty
     conf.sh("kill -USR1 $(pgrep -a kitty) | true")
