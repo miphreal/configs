1ab190ade9345adde69b957168b893a194a4a7f2
Evgeny Lychkovsky <miphreal@gmail.com>
2023-07-14 03:54:10 +0200

    upd zsh & kitty config

--- macos/kitty/.confbuild.py
+++ macos/kitty/.confbuild.py
@@ -1,18 +1,32 @@
 from pathlib import Path
 
+ZSH_RC = """
+##
+# Kitty integration
+if test -n "$KITTY_INSTALLATION_DIR"; then
+    export KITTY_SHELL_INTEGRATION="enabled"
+    autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+    kitty-integration
+    unfunction kitty-integration
+fi
+"""
 
 def kitty(conf):
     conf(
         kitty_font=conf["../fonts"].FiraCodeRetina,
+        kitty_font_size="11.0",
         kitty_conf=conf["path::{{ user.config }}/kitty/kitty.conf"],
         themes_dir=conf["dir::{{ user.config }}/kitty/kitty-themes"],
         theme="SpaceGray",
+        zsh_rc=ZSH_RC,
     )
 
     conf.render("./kitty.conf.j2", conf.kitty_conf)
 
     if not conf.themes_dir.exists():
-        conf.sh("git clone --depth 1 https://github.com/dexpota/kitty-themes.git {{ themes_dir }}")
+        conf.sh(
+            "git clone --depth 1 https://github.com/dexpota/kitty-themes.git {{ themes_dir }}"
+        )
 
     conf.sh("cp -r {{ current_config_dir }}/icons/* {{ kitty_conf.parent }}")
     conf.sh("cp {{ current_config_dir }}/tab_bar.py {{ kitty_conf.parent }}")
--- macos/zsh/.confbuild.py
+++ macos/zsh/.confbuild.py
@@ -1,5 +1,10 @@
 def main(conf):
+    conf[
+        "brew::zsh-history-substring-search",
+        "brew::zsh-syntax-highlighting",
+        "brew::zsh-autosuggestions",
+    ]
+
     conf.render(".zprofile.j2", "~/.zprofile")
     conf.render(".zshenv.j2", "~/.zshenv")
     conf.render(".zshrc.j2", "~/.zshrc")
-    
