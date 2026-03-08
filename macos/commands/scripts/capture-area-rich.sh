d5cee3ecf934e37386a8a96da0425ebd19931fe2
Evgeny Lychkovsky <miphreal@gmail.com>
2026-03-08 20:57:00 +0100

    add raycast screen capture scripts

---
+++ macos/commands/scripts/capture-area-rich.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Capture screen area (rich)
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📷
+
+open "cleanshot://all-in-one?x=850&y=0&width=0&height=0"
+
---
+++ macos/commands/scripts/capture-area.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Capture screen area
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📷
+
+open "cleanshot://capture-area?action=annotate"
+# open "cleanshot://all-in-one?x=850&y=0&width=0&height=0"
+
